729f3fc076fec51212e01940cd1bd0c01e9a2adf
quinn5moli <quinn.moli@gmail.com>
2021-04-13 21:37:04 -0700

    mvp complete

--- index.js
+++ index.js
@@ -39,16 +39,31 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
-  }
- 
- 
+          function Person(name,age) {
+            this.name = name;
+            this.age = age;
+            this.stomach = [];
+          }
+         
+         
+          
+          Person.prototype.eat = function(someFood) {
+            if (this.stomach.length < 11) {
+              this.stomach.push(someFood);
+            }
+            return this.stomach;
+          }
+        
+          Person.prototype.poop = function() {
+            this.stomach = []
+          }
+          
+          Person.prototype.toString = function() {
+            return `${this.name}, ${this.age}`
+          }
+          
+          //let mary = new Person("Mary", 50); 
 
-  
-  
-  
-  
   /*
     TASK 2
       - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -63,11 +78,22 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
   
-  
+  Car.prototype.fill = function(gallons) {
+    return this.tank = this.tank + gallons;
+  }
+
+  Car.prototype.drive = function (distance) {
+    this.odometer = this.odometer + 1;
+    this.tank = this.tank - 1;
+  }
+
   /*
     TASK 3
       - Write a Baby constructor subclassing Person.
@@ -75,18 +101,24 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+   Person.call(this, name, age,favoriteToy)
+   this.favoriteToy = favoriteToy;
   }
  
-  
+  Baby.prototype = Object.create(Person.prototype)
+
+  Baby.prototype.play = function() {
+    return `Playing with ${this.favoriteToy}`
+  }
+
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. Global Object Binding: When "this" is in the global scope, it is referring to the window/console.
+    2. Implicit Binding: When a function/method is used on an object and "this" is used inside of it, "this" is referring to the object that precedes the dot notation.
+    3. New Binding: When a new object is being created, "this" refers to the new object that was created.
+    4. Explicit Binding: When using "call or "apply," "this" is explicitly defined.
   */
   
   
